5ccaedaead34a18fe9540715354d57372a5e9ea9
Ali Muhtarom <kira.mizuky@gmail.com>
2026-06-29 10:29:13 +0700

    feat: implement database controller for managing MySQL, PostgreSQL, and SQLite databases

--- src/modules/database/database.controller.js
+++ src/modules/database/database.controller.js
@@ -14,39 +14,39 @@ class DatabaseController {
         sqlite: sqliteDbs
       });
     } catch (error) {
-      errorResponse(res, 500, error.message);
+      errorResponse(res, error.message, 500);
     }
   }
 
   async createDatabase(req, res) {
     try {
       const { type, name } = req.body;
-      if (!name) return errorResponse(res, 400, 'Database name is required');
+      if (!name) return errorResponse(res, 'Database name is required', 400);
 
       if (type === 'mysql') await databaseService.createMysqlDatabase(name);
       else if (type === 'postgres') await databaseService.createPgDatabase(name);
       else if (type === 'sqlite') await databaseService.createSqliteDatabase(name);
-      else return errorResponse(res, 400, 'Invalid database type');
+      else return errorResponse(res, 'Invalid database type', 400);
 
       successResponse(res, null, `Database ${name} created successfully`);
     } catch (error) {
-      errorResponse(res, 500, error.message);
+      errorResponse(res, error.message, 500);
     }
   }
 
   async deleteDatabase(req, res) {
     try {
       const { type, name } = req.body;
-      if (!name) return errorResponse(res, 400, 'Database name is required');
+      if (!name) return errorResponse(res, 'Database name is required', 400);
 
       if (type === 'mysql') await databaseService.deleteMysqlDatabase(name);
       else if (type === 'postgres') await databaseService.deletePgDatabase(name);
       else if (type === 'sqlite') await databaseService.deleteSqliteDatabase(name);
-      else return errorResponse(res, 400, 'Invalid database type');
+      else return errorResponse(res, 'Invalid database type', 400);
 
       successResponse(res, null, `Database ${name} deleted successfully`);
     } catch (error) {
-      errorResponse(res, 500, error.message);
+      errorResponse(res, error.message, 500);
     }
   }
 }
